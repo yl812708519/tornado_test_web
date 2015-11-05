ebf11891afb5e993fefb1195d11dfaf66ebc9535
杨璐 <812708519@qq.com>
2015-11-05 09:58:27 +0800

    删除

---
+++ app/handlers/photo.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from app.commons.view_model import ViewModel
+
+from app.handlers.application import BaseHandler
+
+
+class IndexHandler(BaseHandler):
+    def get(self, *args):
+
+        self.render("home/index.html", title='哈哈哈哈')
+
